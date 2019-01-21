6f7d705f863e439eca1c666e0abd15db8e01972e
Berkeley19 <31030980+Berkeley19@users.noreply.github.com>
2019-01-21 18:47:35 +0000

    Rank enum update

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -4,6 +4,20 @@ import java.io.*;
 
 public class Card implements Serializable{
 
+    private static final long serialVersionUID = 100;
 
+    enum Rank{
+
+        TWO(2), THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9),TEN(10),
+        JACK(10), QUEEN(10), KING(10), ACE(11);
+
+        private int rankValue;
+
+        Rank(int value) {
+            this.rankValue = value;
+        }
+        
+    }
+    enum Suit{}
 
 }
