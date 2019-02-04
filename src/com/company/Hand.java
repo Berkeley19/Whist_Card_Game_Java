6ff917272183bc206410414b687e08bf903a8703
Berkeley19 <31030980+Berkeley19@users.noreply.github.com>
2019-02-04 23:34:13 +0000

    Hand class

--- src/com/company/Hand.java
+++ src/com/company/Hand.java
@@ -3,15 +3,41 @@ package com.company;
 import java.util.ArrayList;
 
 public class Hand {
+    private static final long serialVersionUID = 300L;
 
     ArrayList<Card> handOfCards;
+    private int currentDiamondCount;
+    private int currentSpadeCount;
+    private int currentClubCount;
+    private int currentHeartCount;
+    private int currentHandCount;
 
 
     public Hand(){
         handOfCards = new ArrayList<>();
     }
 
-    public Hand(Card arr[]){
+    public Hand(ArrayList<Card> arrayOfCards){
+        handOfCards = new ArrayList<>();
+        //add arrayOfCards to hand
+    }
+
+    public Hand(Hand handOfCards){
+        this.handOfCards = new ArrayList<>();
 
     }
+
+    public void addSingleCard(){
+
+    }
+
+    public void addCardColletion(){
+
+    }
+
+    public void addHand(){
+
+    }
+
+
 }
